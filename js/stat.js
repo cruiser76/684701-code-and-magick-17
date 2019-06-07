bd2a2a1028e5c176064c99005d1ee2fae0c3007b
cruiser76 <aleksan76@mail.ru>
2019-06-07 20:00:21 +0600

    исправил замечания робота

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,4 @@
+'use strict';
 var WINDOW_WIDTH = 420;
 var WINDOW_HEIGHT = 270;
 var WINDOW_X = 100;
@@ -8,11 +9,10 @@ var COL_WIDTH = 40;
 var COL_HEIGHT = 150;
 var COL_GAP = 50;
 
-
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, WINDOW_WIDTH, WINDOW_HEIGHT);
-}
+};
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, WINDOW_X + GAP, WINDOW_Y + GAP, 'rgba(0, 0, 0, 0.7)');
@@ -23,21 +23,25 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', WINDOW_X + FONT_GAP, 2 * FONT_GAP);
   ctx.fillText('Список результатов:', WINDOW_X + FONT_GAP, 3 * FONT_GAP);
 
-  var maxTime = Math.max(...times);
+  var maxTime = times.reduce(function (acc, value) {
+    return acc > value ? acc : value;
+  }, 0);
 
   names.forEach(
-    function (player, index) {
-      ctx.fillStyle = 'black';
-      ctx.fillText(player, (WINDOW_X + FONT_GAP) + (COL_WIDTH + COL_GAP) * index, WINDOW_HEIGHT);
+      function (player, index) {
+        ctx.fillStyle = 'black';
+        ctx.fillText(player, (WINDOW_X + FONT_GAP) + (COL_WIDTH + COL_GAP) * index, WINDOW_HEIGHT);
 
-      ctx.fillText(Math.round(times[index]), (WINDOW_X + FONT_GAP) + (COL_WIDTH + COL_GAP) * index,
-        (WINDOW_HEIGHT - FONT_GAP -GAP )  - (times[index] * COL_HEIGHT / maxTime));
+        ctx.fillText(Math.round(times[index]), (WINDOW_X + FONT_GAP) + (COL_WIDTH + COL_GAP) * index,
+            (WINDOW_HEIGHT - FONT_GAP - GAP) - (times[index] * COL_HEIGHT / maxTime));
 
-      ctx.fillStyle = `rgba(0, 0, 255, ${Math.random()})`;
+        ctx.fillStyle = 'rgba(0, 0, 255,' + (Math.random() * (1 - 0.1) + 0.1) + ')';
 
-      if (player === 'Вы') {ctx.fillStyle = 'rgba(255, 0, 0, 1)'}
+        if (player === 'Вы') {
+          ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+        }
 
-      ctx.fillRect((WINDOW_X + FONT_GAP) + (COL_WIDTH + COL_GAP) * index,
-        (WINDOW_HEIGHT - FONT_GAP)  - (times[index] * COL_HEIGHT / maxTime), COL_WIDTH,  times[index] * COL_HEIGHT / maxTime)
-  });
-}
+        ctx.fillRect((WINDOW_X + FONT_GAP) + (COL_WIDTH + COL_GAP) * index,
+            (WINDOW_HEIGHT - FONT_GAP) - (times[index] * COL_HEIGHT / maxTime), COL_WIDTH, times[index] * COL_HEIGHT / maxTime);
+      });
+};
