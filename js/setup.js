2f725dbfd6311b08d8556d75f107e876b81c442a
cruiser76 <aleksan76@mail.ru>
2019-06-11 19:40:11 +0600

    Третье задание

---
+++ js/setup.js
@@ -0,0 +1,80 @@
+'use strict';
+var setup = document.querySelector('.setup');
+setup.classList.remove('hidden');
+
+var names = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+
+var surnames = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг',
+];
+
+var coatColor = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+
+var eyesColor = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+var createWizard = function () {
+  var wizard = {};
+  wizard.name = names[Math.floor(Math.random() * names.length)] + " " + surnames[Math.floor(Math.random() * surnames.length)];
+  wizard.coatColor = coatColor[Math.floor(Math.random() * coatColor.length)];
+  wizard.eyesColor = eyesColor[Math.floor(Math.random() * eyesColor.length)];
+
+  return wizard;
+}
+
+var wizards = [];
+
+for (var i = 0; i < 4; i += 1) {
+  wizards.push(createWizard());
+}
+
+var similarList = document.querySelector('.setup-similar-list');
+
+var wizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+var fragment = document.createDocumentFragment();
+
+var renderWizard = function (wizard) {
+  var wizardItem = wizardTemplate.cloneNode(true);
+  wizardItem.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardItem.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardItem.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  fragment.appendChild(wizardItem);
+};
+
+wizards.forEach(renderWizard);
+
+similarList.appendChild(fragment);
+
+var setupSimilar = document.querySelector('.setup-similar');
+setupSimilar.classList.remove('hidden');
